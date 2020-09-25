3936629053b8598c3354ad5a4000b10fa9d0840e
alikaraa <alikaraa61@outlook.com>
2020-09-25 17:06:43 +0300

    Initial Commit

---
+++ simpleCalculater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleCalculater
+//
+//  Created by Ali Kara on 25.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
